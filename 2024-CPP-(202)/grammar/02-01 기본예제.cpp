6b02f384ec680c2f3bcea6c018423cd791710421
박지영 <s2325@e-mirim.hs.kr>
2024-03-21 11:56:59 +0900

    02-01 이름공간

--- 2024-CPP-(202)/grammar/02-01 기본예제.cpp
+++ 2024-CPP-(202)/grammar/02-01 기본예제.cpp
@@ -1,6 +1,9 @@
 ﻿#include <iostream>
+
+using namespace std; //이름공간. std::를 안해도 된다.
+
 int main(void) {
 	//cout : 출력객체, endl : 개행객체
-	std::cout << "Hello World "<<std::endl;
+	cout << "Hello World "<< endl;
 	return 0;
 } 
