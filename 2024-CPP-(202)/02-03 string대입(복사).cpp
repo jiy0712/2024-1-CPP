de6e4e2b469eeef69eec3e65e2e15789691b6393
박지영 <s2325@e-mirim.hs.kr>
2024-04-04 11:47:26 +0900

    string 대입3(C++방식)

--- 2024-CPP-(202)/02-03 string대입(복사).cpp
+++ 2024-CPP-(202)/02-03 string대입(복사).cpp
@@ -1,48 +1,12 @@
-#include <stdio.h>
-#include <cstdbool>
-#include <string.h>
+#include <iostream>
+#include <string>
 
 int main(void) {
-	char str[30] = "develop hungry";
-	char str2[30];
-	//str에 있는 내용을 str2에 복사
-
-	for (int i = 0; i <= 29; i++) {
-		char str2 = str[i];
-	}
-	printf("%s", str2);
-	//널문자를 안주어서 나오긴 한데 한자가 나온다.
-	
-	//str에 있는 내용을 str2에 복사
-	//"\0" 을 만날때까지
-	int i = 0;
-	for (; str[i] != '\0'; i++) {
-		str2[i] = str[i];
-	}
-	str2[i] = str[i]; //널문자('\0') 복사 , 마지막은 널문자로 끝나야한다.
-	printf("%s\n", str2);
+	std::string str = "develop hungry";
+	std::string str2;
 
 	//str에 있는 내용을 str2에 복사
-	strcpy(str2, str);
+	str2 = str;
 
-	str2[0] = str[0];
-	str2[1] = str[1];
-	str2[2] = str[2];
-	str2[3] = str[3];
-	str2[4] = str[4];
-	str2[5] = str[5];
-	str2[6] = str[6];
-	str2[7] = str[7];
-	str2[8] = str[8];
-	str2[9] = str[9];
-	str2[10] = str[10];
-	str2[11] = str[11];
-	str2[12] = str[12];
-	str2[13] = str[13];
-	str2[14] = str[14];
-	str2[15] = str[15];
-
-	printf("%s\n", str2);
-
-
+	std::cout<<str2;
 }
