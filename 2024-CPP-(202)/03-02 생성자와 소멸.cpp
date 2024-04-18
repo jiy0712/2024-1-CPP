a8848627940b4f04d816443396c049fa6e57ed23
박지영 <s2325@e-mirim.hs.kr>
2024-04-18 11:44:06 +0900

    03-02 생성자, 소멸자 호출시

--- 2024-CPP-(202)/03-02 생성자와 소멸.cpp
+++ 2024-CPP-(202)/03-02 생성자와 소멸.cpp
@@ -11,13 +11,15 @@ public:
 		//'/0'가 들어가기 위해 공간 하나 더 추가
 		int length = strlen(name);
 		name_ = new char[length + 1];
-
+		
 		strcpy(name_, name);
+		cout << "생성자 호출 완료" << endl;
 	}
 
 	//소멸자 : 객체가 소멸 될 때 (메모리에서 지워질 때) 호출되는 함수
 	~Student(void) {
 		delete[]name_;
+		cout << "소멸자 호출 완료" << endl;
 
 	}
 
@@ -34,9 +36,10 @@ private:
 };
 int main(void) {
 	Student* stu = new Student(2213, (char*)"조수빈");
-	stu->show();
-
+	cout << "I'm still hungry - 거스 히딩크" << endl;
 	delete stu;
+
+	cout << "It ain't over, till it's over - 요기 배라" << endl;
 
 	return 0;
 }
