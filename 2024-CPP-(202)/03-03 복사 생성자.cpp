3d85d9220c2c1b92ac2bbdab21237c640fd6770e
박지영 <s2325@e-mirim.hs.kr>
2024-05-07 16:04:07 +0900

    03-03 깊은복사

--- 2024-CPP-(202)/03-03 복사 생성자.cpp
+++ 2024-CPP-(202)/03-03 복사 생성자.cpp
@@ -9,7 +9,7 @@ public:
 
 	Student(int hakbun, char* name) : hakbun_(hakbun) {
 		//'/0'가 들어가기 위해 공간 하나 더 추가
-		int length = strlen(name);
+		int length = strlen(name);	
 		name_ = new char[length + 1];
 
 		strcpy(name_, name);
@@ -17,15 +17,18 @@ public:
 	}
 
 	//복사생성자
-	Student(const Student& rhs) : hakbun_(rhs.hakbun_), name_(rhs.name_)
+	Student(const Student& rhs) : hakbun_(rhs.hakbun_)
 	{
+		int length = strlen(rhs.name_);
+		name_ = new char[length + 1];
+		strcpy(name_, rhs.name_);
+
 		cout << "복사생성자 호출 완료" << endl;
 	}
 
 	//소멸자 : 객체가 소멸 될 때 (메모리에서 지워질 때) 호출되는 함수
 	~Student(void) {
 
-		//TODO : 얕은 복사로 인하여 이미 제거된 공간을 또 제거하려고 해서 에러 발생
 		delete[]name_;
 		cout << "소멸자 호출 완료" << endl;
 
@@ -49,6 +52,5 @@ int main(void) {
 	//복사생성자
 	Student su2 = stu;
 	
-	//TODO : 얕은 복사로 인하여 같은 주소(stu.name_, stu2.name_)에서 두 번 delete 시도
 	return 0;
 }
