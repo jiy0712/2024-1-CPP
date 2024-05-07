f3a1705a1a6bbcb5cc75b860773c7bea4b85bbff
박지영 <s2325@e-mirim.hs.kr>
2024-05-07 15:53:16 +0900

    03-03 얕은복사2

--- 2024-CPP-(202)/03-03 복사 생성자.cpp
+++ 2024-CPP-(202)/03-03 복사 생성자.cpp
@@ -16,8 +16,16 @@ public:
 		cout << "생성자 호출 완료" << endl;
 	}
 
+	//복사생성자
+	Student(const Student& rhs) : hakbun_(rhs.hakbun_), name_(rhs.name_)
+	{
+		cout << "복사생성자 호출 완료" << endl;
+	}
+
 	//소멸자 : 객체가 소멸 될 때 (메모리에서 지워질 때) 호출되는 함수
 	~Student(void) {
+
+		//TODO : 얕은 복사로 인하여 이미 제거된 공간을 또 제거하려고 해서 에러 발생
 		delete[]name_;
 		cout << "소멸자 호출 완료" << endl;
 
@@ -36,12 +44,11 @@ private:
 };
 int main(void) {
 	//일반적인생성자
-	Student stu = Student(2213, (char*)"조수빈");
+	Student stu = Student(2213, (char*)"JSB");
 
 	//복사생성자
 	Student su2 = stu;
 	
 	//TODO : 얕은 복사로 인하여 같은 주소(stu.name_, stu2.name_)에서 두 번 delete 시도
-
 	return 0;
 }
