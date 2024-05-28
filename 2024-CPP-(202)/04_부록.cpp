3feefb825c6dc56e72cb1ce4c9c4b4fdf635cfdc
박지영 <s2325@e-mirim.hs.kr>
2024-05-28 16:08:45 +0900

    05-01 부모/자식 생성/소멸자 호출 순서

--- 2024-CPP-(202)/04_부록.cpp
+++ 2024-CPP-(202)/04_부록.cpp
@@ -5,14 +5,14 @@ using namespace std;
 class Person {
 public:
 	Person(string name, int age)
-		: name_(name), age_(age){}
-
-	void ShowPerson() {
-		cout << name_ << "의 나이는 " << age_ << "입니다." << endl;
+		: name_(name), age_(age){
+		cout << "부모생성자 호출" << endl;
+	}
+	~Person(){
+		cout << "부모소멸자 호출" << endl;
 	}
 	void ShowPerson() {
-		cout << id_;
-		Person::ShowPerson();
+		cout << name_ << "의 나이는 " << age_ << "입니다." << endl;
 	}
 
 private:
@@ -25,7 +25,10 @@ class Student : public Person {
 public:
 	//부모생성자가 먼저 호출
 	Student(string name, int age, int id) : Person(name, age), id_(id) {
-
+		cout << "자식생성자 호출" << endl;
+	}
+	~Student(){
+		cout << "자식소멸자 호출" << endl;
 	}
 	void show_person() {
 		cout << id_ << " ";
@@ -39,5 +42,6 @@ int main(void) {
 
 	Student* stu = new Student("조드리헵번", 39, 1004);
 	stu->show_person();
+	delete stu;
 	return 0;
 }
