6a799375cb0870c56b52e3c5b13958fd6fbf7e53
박지영 <s2325@e-mirim.hs.kr>
2024-05-30 12:06:09 +0900

    05-02 상속예제

--- 2024-CPP-(202)/04_부록.cpp
+++ 2024-CPP-(202)/04_부록.cpp
@@ -1,47 +1,37 @@
-#include <string>
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-class Person {
+class Animal {
+protected: // private에서 protected로 변경
+    string name;
+    int age;
+
 public:
-	Person(string name, int age)
-		: name_(name), age_(age){
-		cout << "부모생성자 호출" << endl;
-	}
-	~Person(){
-		cout << "부모소멸자 호출" << endl;
-	}
-	void ShowPerson() {
-		cout << name_ << "의 나이는 " << age_ << "입니다." << endl;
-	}
+    Animal(string _name, int _age) : name(_name), age(_age) {}
 
-private:
-	string name_;
-	int age_;
-
+    virtual void sound() = 0; // 순수 가상 함수로 변경
+    virtual ~Animal() {} // 가상 소멸자 추가
 };
 
-class Student : public Person {
+class Dog : public Animal {
 public:
-	//부모생성자가 먼저 호출
-	Student(string name, int age, int id) : Person(name, age), id_(id) {
-		cout << "자식생성자 호출" << endl;
-	}
-	~Student(){
-		cout << "자식소멸자 호출" << endl;
-	}
-	void show_person() {
-		cout << id_ << " ";
-		Person::ShowPerson(); //부모 클래스의 멤버함수 호출
-}
-private:
-	int id_;
+    Dog(string _name, int _age) : Animal(_name, _age) {}
+
+    void bark() {
+        cout << "왈 왈" << endl;
+    }
+
+    void sound() override {
+        bark();
+    }
 };
 
-int main(void) {
+int main() {
+    Dog myDog("Buddy", 3);
+    
+    myDog.sound();
 
-	Student* stu = new Student("조드리헵번", 39, 1004);
-	stu->show_person();
-	delete stu;
-	return 0;
+    return 0;
 }
--- 2024-CPP-(202)/05-01 상속기본.cpp
+++ 2024-CPP-(202)/05-01 상속기본.cpp
@@ -1,37 +1,47 @@
-#include <iostream>
 #include <string>
-
+#include <iostream>
 using namespace std;
 
-class Animal {
-protected: // private에서 protected로 변경
-    string name;
-    int age;
-
+class Person {
 public:
-    Animal(string _name, int _age) : name(_name), age(_age) {}
+	Person(string name, int age)
+		: name_(name), age_(age){
+		cout << "부모생성자 호출" << endl;
+	}
+	~Person(){
+		cout << "부모소멸자 호출" << endl;
+	}
+	void ShowPerson() {
+		cout << name_ << "의 나이는 " << age_ << "입니다." << endl;
+	}
 
-    virtual void sound() = 0; // 순수 가상 함수로 변경
-    virtual ~Animal() {} // 가상 소멸자 추가
+private:
+	string name_;
+	int age_;
+
 };
 
-class Dog : public Animal {
+class Student : public Person {
 public:
-    Dog(string _name, int _age) : Animal(_name, _age) {}
-
-    void bark() {
-        cout << "왈 왈" << endl;
-    }
-
-    void sound() override {
-        bark();
-    }
+	//부모생성자가 먼저 호출
+	Student(string name, int age, int id) : Person(name, age), id_(id) {
+		cout << "자식생성자 호출" << endl;
+	}
+	~Student(){
+		cout << "자식소멸자 호출" << endl;
+	}
+	void show_person() {
+		cout << id_ << " ";
+		Person::ShowPerson(); //부모 클래스의 멤버함수 호출
+}
+private:
+	int id_;
 };
 
-int main() {
-    Dog myDog("Buddy", 3);
-    
-    myDog.sound();
+int main(void) {
 
-    return 0;
+	Student* stu = new Student("조드리헵번", 39, 1004);
+	stu->show_person();
+	delete stu;
+	return 0;
 }
